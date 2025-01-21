7b9fff6f6227598ab301b26eb44b05321ca85de2
Jashanpreet Kaur Chahal <chahal63@sheridancollege.ca>
2025-01-21 09:20:49 -0500

    added newsitems dynamically

--- src/components/News.js
+++ src/components/News.js
@@ -1,20 +1,29 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
 
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is a news component.
-       <NewsItem />
-       <NewsItem />
-       <NewsItem />
-       <NewsItem />
-       <NewsItem />
-       <NewsItem />
+      <div className="container my-3">
+        <h2>NewsApp-Top Headlines</h2>
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItem title="myTitle1" description="my description" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="myTitle2" description="my description" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="myTitle3" description="my description" />
+          </div>
+        </div>
+
+        <NewsItem />
+        <NewsItem />
+        <NewsItem />
       </div>
-    )
+    );
   }
 }
 
-export default News
+export default News;
