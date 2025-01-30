f26ba735a021c9211851b7190e6830d5d2c672c0
Jashanpreet Kaur Chahal <chahal63@sheridancollege.ca>
2025-01-30 09:14:57 -0500

    added routing for categories

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 // creating class based component:
 
@@ -16,9 +23,21 @@ export default class App extends Component {
   render() {
     return (
       <>
+      <Router>
       <Navbar />
-      <News pageSize={5} country="us" category="science" />
       
+      <Switch>
+        <Route exact path="/"><News key="general" pageSize={5} country="us" category="general" /></Route>
+        <Route exact path="/general"><News key="general" pageSize={5} country="us" category="general" /></Route>
+        <Route exact path="/business"><News key="business" pageSize={5} country="us" category="business" /></Route>
+        <Route exact path="/entertainment"><News key="entertainment" pageSize={5} country="us" category="entertainment" /></Route>
+        <Route exact path="/health"><News key="health" pageSize={5} country="us" category="health" /></Route>
+        <Route exact path="/science"><News key="science" pageSize={5} country="us" category="science" /></Route>
+        <Route exact path="/sports"><News key="sports" pageSize={5} country="us" category="sports" /></Route>
+        <Route exact path="/technology"><News key="technology" pageSize={5} country="us" category="technology" /></Route>
+        
+      </Switch>
+      </Router>
       </>
     )
   }
