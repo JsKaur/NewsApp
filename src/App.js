e8969989efcd829ba45f3ee5069bb86eae153339
Jashanpreet Kaur Chahal <chahal63@sheridancollege.ca>
2025-02-06 08:19:08 -0500

    added the top loading bar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import React, { Component } from 'react'
 import NavBar from './components/Navbar';
 import News from './components/News';
+import LoadingBar from "react-top-loading-bar";
+
 import {
   BrowserRouter as Router,
   Routes,
@@ -11,20 +13,33 @@ import {
 
 export default class App extends Component {
    pageSize=15;
+
+   state={
+    progress:0
+   }
+   setProgress =(progress)=>{
+    this.setState({progress:progress})
+   }
   render() {
     return (
       <div>
         <Router>
           <NavBar />
+          <LoadingBar
+        color="#f11946"
+        height={3}
+        progress={this.state.progress}
+       
+      />
           <Routes>
-            <Route exact path="/" element={<News key="general" pageSize={this.pageSize} country="in" category="general" />} />
-            <Route exact path="/business" element={<News key="business" pageSize={this.pageSize} country="in" category="business" />} />
-            <Route exact path="/entertainment" element={<News key="entertainment" pageSize={this.pageSize} country="in" category="entertainment" />} />
-            <Route exact path="/general" element={<News key="general" pageSize={this.pageSize} country="in" category="general" />} />
-            <Route exact path="/health" element={<News key="health" pageSize={this.pageSize} country="in" category="health" />} />
-            <Route exact path="/science" element={<News key="science" pageSize={this.pageSize} country="in" category="science" />} />
-            <Route exact path="/sports" element={<News key="sports" pageSize={this.pageSize} country="in" category="sports" />} />
-            <Route exact path="/technology" element={<News key="technology" pageSize={this.pageSize} country="in" category="technology" />} />
+            <Route exact path="/" element={<News setprogress={this.setProgress}key="general" pageSize={this.pageSize} country="in" category="general" />} />
+            <Route exact path="/business" element={<News setprogress={this.setProgress}key="business" pageSize={this.pageSize} country="in" category="business" />} />
+            <Route exact path="/entertainment" element={<News setprogress={this.setProgress}key="entertainment" pageSize={this.pageSize} country="in" category="entertainment" />} />
+            <Route exact path="/general" element={<News setprogress={this.setProgress}key="general" pageSize={this.pageSize} country="in" category="general" />} />
+            <Route exact path="/health" element={<News setprogress={this.setProgress}key="health" pageSize={this.pageSize} country="in" category="health" />} />
+            <Route exact path="/science" element={<News setprogress={this.setProgress}key="science" pageSize={this.pageSize} country="in" category="science" />} />
+            <Route exact path="/sports" element={<News setprogress={this.setProgress}key="sports" pageSize={this.pageSize} country="in" category="sports" />} />
+            <Route exact path="/technology" element={<News setprogress={this.setProgress}key="technology" pageSize={this.pageSize} country="in" category="technology" />} />
           </Routes>
         </Router>
       </div>
--- src/components/News.js
+++ src/components/News.js
@@ -31,16 +31,20 @@ export class News extends Component {
 
 
     async updateNews(pageNo){
+        this.props.setProgress(10);
         const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=dbe57b028aeb41e285a226a94865f7a7&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         this.setState({loading: true});
         let data = await fetch(url);
+        this.props.setProgress(30);
         let parsedData = await data.json()
+        this.props.setProgress(70);
         console.log(parsedData); 
         this.setState({articles: parsedData.articles,
             totalResults: parsedData.totalResults,
             loading: false,
             
         })
+        this.props.setprogress(100);
 
     }
 
